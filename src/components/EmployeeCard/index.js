0ab6075bca96fccddb05482fdaf4625b0b1d2d83
Pavel Darii <paveldarii@yahoo.com>
2021-03-02 17:48:59 -0500

    changed the className attribute instead of class

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -5,26 +5,27 @@ import SpringModal from "../SpringModal";
 function EmployeeCard(props) {
   return (
     <div>
-      <div class="bg-white card rounded shadow-sm py-5 px-4">
+      <div className="bg-white card rounded shadow-sm py-5 px-4">
         <img
           src={props.image}
           alt={props.name}
           width="100"
-          class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm "
+          className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm "
         ></img>
+        <div className="content">
+          <h5 className="mb-0">{props.name}</h5>
+          <span className="small text-uppercase ">{props.occupation}</span>
+          <p>{`${props.city}, ${props.country}`}</p>
 
-        <h5 class="mb-0">{props.name}</h5>
-        <span class="small text-uppercase ">{props.occupation}</span>
-        <p>{`${props.city}, ${props.country}`}</p>
-
-        <SpringModal
-          image={props.image}
-          name={props.name}
-          occupation={props.occupation}
-          location={props.location}
-          email={props.email}
-          phone={props.phone}
-        ></SpringModal>
+          <SpringModal
+            image={props.image}
+            name={props.name}
+            occupation={props.occupation}
+            location={props.location}
+            email={props.email}
+            phone={props.phone}
+          ></SpringModal>
+        </div>
       </div>
     </div>
   );
