774a2c4a234786c164bb2ad18af6835e66fb94ca
Pavel Darii <paveldarii@yahoo.com>
2021-03-02 13:54:52 -0500

    created first succesfull version of displaying employee list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import employees from "./employees.json";
+import employees from "./new-employees.json";
 
 class App extends Component {
   // Setting this.state.employees to the employees json array
@@ -13,26 +13,28 @@ class App extends Component {
   removeEmployee = (id) => {
     // Filter this.state.emmployees for employees with an id not equal to the id being removed
     const employees = this.state.employees.filter(
-      (employee) => employee.id !== id
+      (employee) => employee.info.seed !== id
     );
     // Set this.state.employees equal to the new employees array
     this.setState({ employees });
   };
-
   // Map over this.state.employees and render a EmployeeCard component for each employee object
   render() {
+    console.log(employees);
     return (
       <Wrapper>
         <Title>Employee List</Title>
         {this.state.employees.map((employee) => (
           <EmployeeCard
             removeEmployee={this.removeEmployee}
-            id={employee.id}
-            key={employee.id}
-            name={employee.name}
-            image={employee.image}
-            occupation={employee.occupation}
-            location={employee.location}
+            id={employee.info.seed}
+            key={employee.info.seed}
+            name={
+              employee.results[0].name.first + employee.results[0].name.last
+            }
+            image={employee.results[0].picture.large}
+            occupation={employee.job}
+            location={`${employee.results[0].location.street.number} ${employee.results[0].location.street.name}, ${employee.results[0].location.city}, ${employee.results[0].location.state}, ${employee.results[0].location.country}`}
           />
         ))}
       </Wrapper>
