f61e3f08474d29c717c4d2d07e65f4a64d80b4f8
Pavel Darii <paveldarii@yahoo.com>
2021-03-04 17:07:02 -0500

    added sortBy function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
+import SortByForm from "./components/SortByForm";
 import employees from "./new-employees.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,6 +11,11 @@ class App extends Component {
   state = {
     employees,
     currentNavItem: "",
+    currentSortItem: "",
+  };
+
+  handleSortByChange = (sortItem) => {
+    this.setState({ currentSortItem: sortItem });
   };
 
   handleNavItemChange = (navItem) => {
@@ -17,11 +23,39 @@ class App extends Component {
   };
   renderSearchSection = () => {
     if (this.state.currentNavItem === "Sort By") {
-      return <p>Sort by</p>;
+      return (
+        <SortByForm handleSortByChange={this.handleSortByChange()}></SortByForm>
+      );
     } else if (this.state.currentNavItem === "Filter By") {
       return <p>Filter by</p>;
     } else {
       return "";
+    }
+  };
+
+  sortBy = () => {
+    if (this.state.currentSortItem === "Name") {
+      this.state.employee.sort();
+      // Set this.state.employees equal to the new employees array
+      this.setState({ employees });
+    } else if (this.state.currentSortItem === "Years of Experience") {
+      return;
+    } else {
+      this.state.employees.map((employee) => (
+        <EmployeeCard
+          removeEmployee={this.removeEmployee}
+          id={employee.info.seed}
+          key={employee.info.seed}
+          name={`${employee.results[0].name.first} ${employee.results[0].name.last}`}
+          image={employee.results[0].picture.large}
+          occupation={employee.job}
+          location={`${employee.results[0].location.street.number} ${employee.results[0].location.street.name}, ${employee.results[0].location.city}, ${employee.results[0].location.state}, ${employee.results[0].location.country}, ${employee.results[0].location.postcode}`}
+          phone={employee.results[0].phone}
+          email={employee.results[0].email}
+          city={employee.results[0].location.city}
+          country={employee.results[0].location.country}
+        />
+      ));
     }
   };
   removeEmployee = (id) => {
