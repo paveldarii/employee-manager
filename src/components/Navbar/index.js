51c9d97aba598bdbd092bcaef6b5758c41a49ade
Pavel Darii <paveldarii@yahoo.com>
2021-03-04 11:41:40 -0500

    added two nav items to the nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./style.css";
-
 function Navbar(props) {
   return (
     <nav className="navbar navbar-light bg-light">
       <img src="./logo.png" width="20%" height="20%" alt="logo"></img>
 
-      <ul className="navbar-nav mr-auto"></ul>
+      <ul className="navbar-nav mr-auto">
+        <li
+          className="nav-item"
+          onClick={() => props.handleNavItemChange("Filter By")}
+          className={
+            props.currentNavItem === "Filter By"
+              ? "nav-link active"
+              : "nav-link"
+          }
+        >
+          Filter By
+        </li>
+        <li className="nav-item">
+          <a
+            onClick={() => props.handleNavItemChange("Sort By")}
+            className={
+              props.currentNavItem === "Sort By"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Sort By
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
