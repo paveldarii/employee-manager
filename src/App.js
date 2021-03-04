546ed7ce284e29b7c5b6b75f56ef20c906b58c2a
Pavel Darii <paveldarii@yahoo.com>
2021-03-03 21:55:06 -0500

    inserted Navbar component on the top of all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 import employees from "./new-employees.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -22,24 +22,26 @@ class App extends Component {
   // Map over this.state.employees and render a EmployeeCard component for each employee object
   render() {
     return (
-      <Wrapper>
-        <Title>Employee Directory</Title>
-        {this.state.employees.map((employee) => (
-          <EmployeeCard
-            removeEmployee={this.removeEmployee}
-            id={employee.info.seed}
-            key={employee.info.seed}
-            name={`${employee.results[0].name.first} ${employee.results[0].name.last}`}
-            image={employee.results[0].picture.large}
-            occupation={employee.job}
-            location={`${employee.results[0].location.street.number} ${employee.results[0].location.street.name}, ${employee.results[0].location.city}, ${employee.results[0].location.state}, ${employee.results[0].location.country}, ${employee.results[0].location.postcode}`}
-            phone={employee.results[0].phone}
-            email={employee.results[0].email}
-            city={employee.results[0].location.city}
-            country={employee.results[0].location.country}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <Navbar></Navbar>
+        <Wrapper>
+          {this.state.employees.map((employee) => (
+            <EmployeeCard
+              removeEmployee={this.removeEmployee}
+              id={employee.info.seed}
+              key={employee.info.seed}
+              name={`${employee.results[0].name.first} ${employee.results[0].name.last}`}
+              image={employee.results[0].picture.large}
+              occupation={employee.job}
+              location={`${employee.results[0].location.street.number} ${employee.results[0].location.street.name}, ${employee.results[0].location.city}, ${employee.results[0].location.state}, ${employee.results[0].location.country}, ${employee.results[0].location.postcode}`}
+              phone={employee.results[0].phone}
+              email={employee.results[0].email}
+              city={employee.results[0].location.city}
+              country={employee.results[0].location.country}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
