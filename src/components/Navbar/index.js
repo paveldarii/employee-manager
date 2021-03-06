25f758d06fe3617573356fc52c6a641d76ab1551
Pavel Darii <paveldarii@yahoo.com>
2021-03-05 22:22:07 -0500

    changed the structure of the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,42 @@
 import React from "react";
 import "./style.css";
+
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <img src="./logo.png" width="20%" height="20%" alt="logo"></img>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <img
+          id="logo"
+          src="./logo.png"
+          alt="logo"
+          onClick={() => {
+            props.resetTheScreen();
+          }}
+        ></img>
 
-      <ul className="navbar-nav mr-auto">
-        <li
-          className="nav-item"
-          onClick={() => props.handleNavItemChange("Filter By")}
-          className={
-            props.currentNavItem === "Filter By"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Filter By
-        </li>
-        <li
-          className="nav-item"
-          onClick={() => props.handleNavItemChange("Sort By")}
-          className={
-            props.currentNavItem === "Sort By" ? "nav-link active" : "nav-link"
-          }
-        >
-          Sort By
-        </li>
-      </ul>
+        <div className=" justify-content-end" id="navbarNav">
+          <form class="form-inline">
+            <button
+              class="btn btn-sm btn-outline-secondary"
+              type="button"
+              onClick={() => {
+                props.handleNavStatus("sort");
+              }}
+            >
+              Sort
+            </button>
+            <button
+              class="btn btn-sm btn-outline-secondary"
+              onClick={() => {
+                props.handleNavStatus("filter");
+              }}
+              type="button"
+            >
+              Filter
+            </button>
+          </form>
+        </div>
+      </div>
     </nav>
   );
 }
