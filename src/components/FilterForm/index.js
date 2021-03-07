a480dee9dea2049cf134353f4124e7ab0757cb5e
Pavel Darii <paveldarii@yahoo.com>
2021-03-06 22:03:00 -0500

    created the search form when filtering by name

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./style.css";
 
 function FilterForm(props) {
-  return <p>filter</p>;
+  return (
+    <div className="input-group">
+      <input
+        type="search"
+        className="form-control rounded"
+        placeholder="Search Name"
+        aria-label="Search"
+        aria-describedby="search-addon"
+      />
+      <button type="button" className="btn btn-outline-info">
+        search
+      </button>
+    </div>
+  );
 }
 
 export default FilterForm;
